ceed3a9e9466802b3a55fbd1b9e7e527a6d3021a
zzy <975722423@qq.com>
2020-07-31 22:30:19 +0800

    添加网关

--- springcloud-zuul-9527/src/main/java/com/boss/springcloud/ZuulApplication_9527.java
+++ springcloud-zuul-9527/src/main/java/com/boss/springcloud/ZuulApplication_9527.java
@@ -2,12 +2,12 @@ package com.boss.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderConsumer_80 {
+@EnableZuulProxy
+public class ZuulApplication_9527 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderConsumer_80.class,args);
+        SpringApplication.run(ZuulApplication_9527.class,args);
     }
 }
